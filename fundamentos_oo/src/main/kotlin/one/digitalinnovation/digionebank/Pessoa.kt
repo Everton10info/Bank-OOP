12ad763a60fa4fa039d4b63f190d66eda9fd4737
Everton Bellmann <everton10info@gmail.com>
2021-03-14 17:18:02 -0300

    changed the visibily of set of the person class to private

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,11 +3,16 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String = "Jether"
     var cpf:String = "123.123.123.12"
+    private set
+
 }
 
 
 fun main(){
     val  Jether = Pessoa()
+    Jether.cpf = "1245.1287"
     println(Jether.nome)
     println(Jether.cpf)
+
+
 }
