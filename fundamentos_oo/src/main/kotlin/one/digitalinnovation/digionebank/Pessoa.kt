da3e596c53b523dc1a222f2e0afb2d6990563d33
Everton Bellmann <everton10info@gmail.com>
2021-03-15 22:41:33 -0300

    update to make and employee abstract class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,20 +1,11 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome:String = "Jether"
-    var cpf:String = "123.123.123.12"
-    private set
-    constructor()
-
-    fun pessoainfo()= "$nome e  $cpf"
-
-}
-
-
-fun main(){
-    val  Jether = Pessoa()
-    println(Jether.pessoainfo())
+abstract class Pessoa (
+    val nome:String,
+    val  cpf:String
+)
 
 
 
-}
+
+
